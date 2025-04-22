60371c014775d306a377db4df0c0bd0881165332
Vinicius Mota <vinicius-motta2011@hotmail.com>
2025-04-21 22:23:17 -0300

    add a variant option to the header link component

--- src/components/HeaderLink/HeaderLink.tsx
+++ src/components/HeaderLink/HeaderLink.tsx
@@ -1,11 +1,19 @@
+import clsx from "clsx";
+
 interface HeaderLinkProps {
+  variant: "header" | "header-menu";
   to: string;
   label: string;
 }
 
-const HeaderLink = ({ to, label }: HeaderLinkProps) => {
+const HeaderLink = ({ variant, to, label }: HeaderLinkProps) => {
+  const variantStyles = {
+    header: "text-[15px] font-bold text-header-link hover:text-dark",
+    "header-menu": "text-[18px] font-bold text-white",
+  };
+
   return (
-    <a href={to} className="text-[15px] font-bold text-header-link hover:text-dark">
+    <a href={to} className={clsx(variantStyles[variant])}>
       {label}
     </a>
   );
